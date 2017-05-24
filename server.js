6dd3898014888ec408dbd9f07ef974b727277abd
william <william.grisa@bravi.com.br>
2017-05-24 16:54:15 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 1 + 1 };
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
